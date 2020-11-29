0e81698f9b9f63d88c9057961ba768426a6d6d00
Eric Ampire <ericampire.top@gmail.com>
2020-11-29 10:53:20 +0200

    Ktor module Dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,6 +5,7 @@ object Version {
     const val compose = "1.0.0-alpha07"
     const val kotlin = "1.4.10"
     const val accompanist = "0.3.3.1"
+    const val koin = "2.1.6"
 }
 
 object Deps {
@@ -12,6 +13,25 @@ object Deps {
     object Gradle {
         const val plugin = "com.android.tools.build:gradle:4.2.0-alpha15"
     }
+
+    object Koin {
+        const val android = "org.koin:koin-android:${Version.koin}"
+        const val scope = "org.koin:koin-androidx-scope:${Version.koin}"
+        const val viewModel = "org.koin:koin-androidx-viewmodel:${Version.koin}"
+        const val fragment = "org.koin:koin-androidx-fragment:${Version.koin}"
+        const val test = "org.koin:koin-test:${Version.koin}"
+    }
+
+    object Ktor {
+        private const val version = "1.4.0"
+
+        const val core = "io.ktor:ktor-client-core:$version"
+        const val android = "io.ktor:ktor-client-android:$version"
+        const val cio = "io.ktor:ktor-client-cio:$version"
+        const val serialization = "io.ktor:ktor-client-serialization-jvm:$version"
+        const val logging = "io.ktor:ktor-client-logging-jvm:$version"
+    }
+
 
     object Accompanist {
         const val glide = "dev.chrisbanes.accompanist:accompanist-glide:${Version.accompanist}"
@@ -40,6 +60,16 @@ object Deps {
         const val appCompat = "androidx.appcompat:appcompat:1.2.0"
         const val composeNavigation = "androidx.navigation:navigation-compose:1.0.0-alpha01"
         const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:2.3.0-beta01"
+
+        object Test {
+            const val ext = "androidx.test.ext:junit:1.1.1"
+            const val junit = "junit:junit:4.13"
+
+            object Expresso {
+                private const val version = "3.2.0"
+                const val core = "androidx.test.espresso:espresso-core:$version"
+            }
+        }
 
 
         object Compose {
