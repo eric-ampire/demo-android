7551ec9d22dde688dd959f4567f4fb5351d8ae3e
Eric Ampire <ericampire.top@gmail.com>
2020-11-29 11:00:10 +0200

    Configure Ktor client

--- ktor-demo/src/main/java/com/ericampire/android/ktordemo/app/KoinModule.kt
+++ ktor-demo/src/main/java/com/ericampire/android/ktordemo/app/KoinModule.kt
@@ -1,5 +1,10 @@
 package com.ericampire.android.ktordemo.app
 
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
 import org.koin.dsl.module
 
 val repositoryModule = module {
@@ -7,5 +12,17 @@ val repositoryModule = module {
 }
 
 val netModule = module {
+    fun provideKoinClient(): HttpClient {
+        return HttpClient(Android) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer()
+            }
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.HEADERS
+            }
+        }
+    }
 
+    single { provideKoinClient() }
 }
