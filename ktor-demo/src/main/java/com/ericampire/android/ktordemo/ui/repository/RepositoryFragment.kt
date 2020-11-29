6c1fa7f9202f1c51e7e711d36dc92973b748982e
Eric Ampire <ericampire.top@gmail.com>
2020-11-29 11:19:32 +0200

    Adding repository fragment

---
+++ ktor-demo/src/main/java/com/ericampire/android/ktordemo/ui/repository/RepositoryFragment.kt
@@ -0,0 +1,15 @@
+package com.ericampire.android.ktordemo.ui.repository
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ericampire.android.ktordemo.R
+
+
+class RepositoryFragment : Fragment(R.layout.fragment_repository) {
+
+
+
+}
