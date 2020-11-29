28855a1c43c11b3efbf372a2da1a19a59e3c8521
Eric Ampire <ericampire.top@gmail.com>
2020-11-29 11:22:42 +0200

    Adding ListRepo ViewModel et Repository

---
+++ ktor-demo/src/main/java/com/ericampire/android/ktordemo/ui/repository/ListRepoRepository.kt
@@ -0,0 +1,7 @@
+package com.ericampire.android.ktordemo.ui.repository
+
+import io.ktor.client.*
+
+class ListRepoRepository(private val httpClient: HttpClient) {
+
+}
---
+++ ktor-demo/src/main/java/com/ericampire/android/ktordemo/ui/repository/ListRepoViewModel.kt
@@ -0,0 +1,7 @@
+package com.ericampire.android.ktordemo.ui.repository
+
+import androidx.lifecycle.ViewModel
+
+class ListRepoViewModel(private val repository: ListRepoRepository) : ViewModel() {
+
+}
