2edfcc1f07f1a5272cd81f57d1a3b051f226880e
Eric Ampire <ericampire.top@gmail.com>
2020-11-29 10:56:12 +0200

    Setup Koin

---
+++ ktor-demo/src/main/java/com/ericampire/android/ktordemo/app/App.kt
@@ -0,0 +1,19 @@
+package com.ericampire.android.ktordemo.app
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setupKoinModule()
+    }
+
+    private fun setupKoinModule() {
+        startKoin {
+            androidContext(this@App)
+            modules(repositoryModule, netModule)
+        }
+    }
+}
---
+++ ktor-demo/src/main/java/com/ericampire/android/ktordemo/app/KoinModule.kt
@@ -0,0 +1,11 @@
+package com.ericampire.android.ktordemo.app
+
+import org.koin.dsl.module
+
+val repositoryModule = module {
+
+}
+
+val netModule = module {
+
+}
