d9c59f5384c308d994b25466635c5b38276d9e14
Eric Ampire <ericampire.top@gmail.com>
2020-11-29 11:17:22 +0200

    The entity that the Api will return

---
+++ ktor-demo/src/main/java/com/ericampire/android/ktordemo/entity/GithubApiEntity.kt
@@ -0,0 +1,24 @@
+package com.ericampire.android.ktordemo.entity
+
+import kotlinx.serialization.*
+
+@Serializable
+data class Example (
+    @SerialName("id") var id : Int,
+    @SerialName("node_id") var nodeId : String,
+    @SerialName("name") var name : String,
+    @SerialName("full_name") var fullName : String,
+    @SerialName("private") var private : Boolean,
+    @SerialName("owner") var owner : Owner,
+    @SerialName("description") var description : String,
+    @SerialName("fork") var fork : Boolean,
+)
+
+@Serializable
+data class Owner (
+    @SerialName("login") var login : String,
+    @SerialName("id") var id : Int,
+    @SerialName("node_id") var nodeId : String,
+    @SerialName("avatar_url") var avatarUrl : String,
+    @SerialName("gravatar_id") var gravatarId : String,
+)
